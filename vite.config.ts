daf16a081aaec083ff719e5b5f23e6fe802db975
haroonth <kittisak.ma@logicspark.com>
2023-04-17 12:23:28 +0700

    🐛 inject css

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 // import { defineConfig } from "vite";
 import { defineConfig } from "vitest/config";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), cssInjectedByJsPlugin()],
 	css: {},
 	test: {
 		globals: true,
